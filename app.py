eaec07839c6fc2de82f890fefd614f1dd9f9000e
Zpoetri01 <162399144+Zpoetri01@users.noreply.github.com>
2025-12-18 20:29:41 +0700

    Initialize Flask application with a home route

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os, socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"Hello from instance: {socket.gethostname()}"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
